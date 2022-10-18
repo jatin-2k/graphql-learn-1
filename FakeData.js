71babcd7e30428d1ed01b4b93bd59e38dd7f73b7
Jatin Agrawal <jatinagrawal2000@gmail.com>
2022-10-18 21:27:05 +0530

    Basic graphQL server with apollo

---
+++ FakeData.js
@@ -0,0 +1,74 @@
+const UserList = [
+     {
+      "id": 1,
+      "name": "Kristin Green",
+      "username": "Patty54",
+      "age": 36,
+      "nationality": "russian"
+    },
+    {
+      "id": 2,
+      "name": "Mckinney Burke",
+      "username": "Patel22",
+      "age": 25,
+      "nationality": "mexican"
+    },
+    {
+      "id": 3,
+      "name": "Sargent Melendez",
+      "username": "Foster30",
+      "age": 33,
+      "nationality": "russian"
+    },
+    {
+      "id": 4,
+      "name": "Rhoda Meyers",
+      "username": "Baldwin38",
+      "age": 28,
+      "nationality": "canadian"
+    },
+    {
+      "id": 5,
+      "name": "Audrey Delacruz",
+      "username": "Carolina23",
+      "age": 24,
+      "nationality": "american"
+    },
+    {
+      "id": 6,
+      "name": "Cobb Webster",
+      "username": "Hill58",
+      "age": 33,
+      "nationality": "american"
+    },
+    {
+      "id": 7,
+      "name": "Lang Hahn",
+      "username": "Enid21",
+      "age": 38,
+      "nationality": "canadian"
+    },
+    {
+      "id": 8,
+      "name": "Matthews Carrillo",
+      "username": "Bowers100",
+      "age": 26,
+      "nationality": "russian"
+    },
+    {
+      "id": 9,
+      "name": "Decker Dotson",
+      "username": "Carney31",
+      "age": 38,
+      "nationality": "canadian"
+    },
+    {
+      "id": 10,
+      "name": "Fannie Woodward",
+      "username": "Hardy42",
+      "age": 33,
+      "nationality": "canadian"
+    }
+]
+
+module.exports = UserList;
